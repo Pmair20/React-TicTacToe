3ca72fa19afeafab525cf305bd32a3a59c903ad5
Pmair20 <pritishmair007@gmail.com>
2022-06-16 12:44:47 +0530

    Updated index with interactivity

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={value:null};
+    }
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button className="square" onClick={() => this.setState({value:'X'})}>
+                {this.state.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        return <Square value= {this.state.squares[i]}/>;
     }
 
     render() {
@@ -37,11 +49,6 @@ class Board extends React.Component {
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(9)}
-                    {this.renderSquare(10)}
-                    {this.renderSquare(11)}
                 </div>
             </div>
         );
